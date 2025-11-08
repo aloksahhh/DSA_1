9abc24329c7af86e4cf10e3d7e926ae62ff7af38
aloksahhh <aloksah048@gmail.com>
2025-11-08 19:10:30 +0530

    two sum problem of leetcode(using two pointers).

---
+++ twoSum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class twoSum {
+    public static void main(String[] args) {
+        int[] arr={2,7,11,14};
+        int target=9;
+        System.out.println(Arrays.toString(sum(arr,target)));
+    }
+    public static int[] sum(int[] arr, int target){
+        int[] ans = new int[2];
+         int end = arr.length-1;
+         for(int i=0;i<arr.length-1;i++){
+            for(int j=end;j>i;j--){
+                if(arr[i]+arr[j] == target){
+                    ans[0]=i;
+                    ans[1]=j;
+                }
+            }
+         }
+         return ans;
+    }
+}
+
