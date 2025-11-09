7b9b5f2d95c4d0ec73147221a86a7572a462f960
aloksahhh <aloksah048@gmail.com>
2025-11-09 20:07:04 +0530

    remove duplicate numbers from array program.

---
+++ removeDuplicate.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class removeDuplicate {
+    public static void main(String[] args) {
+        int[] arr= {0,0,1,1,1,2,2,3,3,4};
+        int[] res = remove(arr);
+        System.out.println(Arrays.toString(res));
+        
+    }
+    public  static int[] remove(int[] nums) {
+        int start=0; int end=1; int len = nums.length;
+        int[] ans=new int[len]; int x=0;
+        while(end<len){
+            if(nums[start]==nums[end]){
+               
+                end++;
+            }
+            else{
+                ans[x]=nums[start];
+                x++;
+                start=end;
+                end++;
+            }
+        }
+        ans[x++]=nums[start];
+        return ans;
+    }
+    
+}
